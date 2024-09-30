869ffd560ffbc44d68c122cd7db700b99d4c2345
Mohammad Adnan <mohammadadnan6460@gmail,com>
2024-09-30 18:21:30 +0530

    sss

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/postx-frontend/', // Ensure this matches your repository name
   plugins: [react()],
-  base: '/postX-frontend/', // Ensure this matches your repository name
 });
